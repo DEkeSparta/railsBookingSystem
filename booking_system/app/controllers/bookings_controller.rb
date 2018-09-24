9c00e88e9ca17fadd8263a7a36faa1e8f7bc1502
DEkeSparta <deke@spartaglobal.com>
2018-09-24 16:02:44 +0100

    Added basic date & time entry forms, added room search feature with test values

--- booking_system/app/controllers/bookings_controller.rb
+++ booking_system/app/controllers/bookings_controller.rb
@@ -15,6 +15,8 @@ class BookingsController < ApplicationController
   # GET /bookings/new
   def new
     @booking = Booking.new
+    @bookings = Booking.all
+    @rooms = Room.all
   end
 
   # GET /bookings/1/edit
@@ -69,6 +71,6 @@ class BookingsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def booking_params
-      params.fetch(:booking, {})
+      params.require(:booking).permit(:date, :start_time, :end_time, :room_id, :description)
     end
 end
--- booking_system/app/models/room.rb
+++ booking_system/app/models/room.rb
@@ -1,2 +1,18 @@
 class Room < ApplicationRecord
+  has_many :bookings
+
+  def isFree date, startT, endT
+    isFree = true
+
+    self.bookings.each do |b|
+      if date == b.date
+        if startT.strftime( "%H%M" ) < b.end.strftime( "%H%M" ) && endT.strftime( "%H%M" ) > b.start.strftime( "%H%M" )
+          isFree = false
+          break
+        end
+      end
+    end
+
+    isFree
+  end
 end
