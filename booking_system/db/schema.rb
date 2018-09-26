436b15005e893945ab0212b9fcefca3b4712e2ff
FarhaanSG <fahmed@spartaglobal.com>
2018-09-26 14:42:42 +0100

    added column category to table bookings

--- booking_system/db/schema.rb
+++ booking_system/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_26_130806) do
+ActiveRecord::Schema.define(version: 2018_09_26_132730) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
