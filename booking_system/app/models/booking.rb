2209383ef8c9e71a9b2781d1a08fe004f38c7e5e
FarhaanSG <fahmed@spartaglobal.com>
2018-09-26 16:20:11 +0100

    table fits the whole page

--- booking_system/app/models/booking.rb
+++ booking_system/app/models/booking.rb
@@ -3,10 +3,10 @@ class Booking < ApplicationRecord
 
   validates :description, presence: true, allow_blank: false
   validate :start_must_be_before_end_time
-  
+
   def start_must_be_before_end_time
     errors.add(:start, "must be before end time") unless
-        self.start.strftime("%H%M") < self.end.strftime("%H%M")
+      self.start.strftime("%H%M") < self.end.strftime("%H%M")
   end
 
 
